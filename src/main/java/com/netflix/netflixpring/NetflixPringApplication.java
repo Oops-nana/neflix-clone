74ece3ea22db07a3da5becc526507365dafd8c69
wonjae <dnjswo6253@naver.com>
2022-03-07 22:42:47 +0900

    init

---
+++ src/main/java/com/netflix/netflixpring/NetflixPringApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.netflixpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixPringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixPringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netflix/netflixpring/NetflixPringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.netflixpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixPringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
